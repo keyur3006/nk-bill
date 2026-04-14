bd8f2ddb05309e5bf305f5deec7a96806a5ef29e
keyurbelltom-byte <keyur.belltom@gmail.com>
2026-04-14 21:21:13 +0530

    aws b

--- nr-foods-frontend/src/utils/api.ts
+++ nr-foods-frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://nr-foods-backend.onrender.com/api",
+  baseURL: "http://3.90.190.155:5000/api"
 });
 
 api.interceptors.request.use((config) => {
