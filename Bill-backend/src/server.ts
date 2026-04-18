3886f3484621edd18fa8f5cccfda014ea663e062
keyurbelltom-byte <keyur.belltom@gmail.com>
2026-04-18 15:12:48 +0530

    fix cors dynamic

--- Bill-backend/src/server.ts
+++ Bill-backend/src/server.ts
@@ -18,20 +18,28 @@ const prisma = new PrismaClient();
 
 /* ================= CORS ================= */
 
-const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://nk-bill.vercel.app",
-    "https://keyurbill.online",
-    "https://www.keyurbill.online",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://nk-bill.vercel.app",
+  "https://keyurbill.online",
+  "https://www.keyurbill.online",
+];
 
-app.use(cors(corsOptions));
-// app.use(cors(corsOptions));// 🔥 VERY IMPORTANT
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // Postman / mobile
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+// 🔥 VERY IMPORTANT
 
 /* ================= MIDDLEWARE ================= */
 
