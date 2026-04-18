ce84e95cf963de250f51aefc5264946afd2eed40
keyurbelltom-byte <keyur.belltom@gmail.com>
2026-04-18 15:00:36 +0530

    final cors fix

--- Bill-backend/src/server.ts
+++ Bill-backend/src/server.ts
@@ -31,7 +31,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions)); // 🔥 VERY IMPORTANT
+// app.use(cors(corsOptions));// 🔥 VERY IMPORTANT
 
 /* ================= MIDDLEWARE ================= */
 
